25bc08e30bd7767c2dad34cc712e8ee286ae0699
jamesjbot <jongs.j@gmail.com>
2016-10-07 04:51:27 -0400

    Initial Commit

---
+++ BreweryTour/BreweryDBClient.swift
@@ -0,0 +1,19 @@
+//
+//  BreweryDBClient.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 10/7/16.
+//  Copyright © 2016 James Jongs. All rights reserved.
+//
+
+import Foundation
+
+class BreweryDBClient {
+    
+}
+
+extension BreweryDBClient {
+    enum Constants {
+        
+    }
+}
