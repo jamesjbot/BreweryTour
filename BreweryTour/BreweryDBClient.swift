d8f62a5f86dc93aea5aa08b13b33c809aadb5e99
jamesjbot <jongs.j@gmail.com>
2016-10-07 22:36:37 -0400

    Analyzing the out put data

--- BreweryTour/BreweryDBClient.swift
+++ BreweryTour/BreweryDBClient.swift
@@ -11,12 +11,74 @@ import Alamofire
 
 class BreweryDBClient {
 
+    func downloadBeerTypes(){
+//        let methodParameters = [
+//            Constants.BreweryParameterKeys.Key.Method : Constants.BreweryParameterValues.SearchMethod,
+//            Constants.BreweryParameterKeys.Format : Constants.BreweryParameterValues.FormatValue,
+//            Constants.FlickrParameterKeys.BoundingBox : boundingboxConstruct(),
+//            Constants.FlickrParameterKeys.SafeSearch : Constants.FlickrParameterValues.UseSafeSearch,
+//            Constants.FlickrParameterKeys.Extras : Constants.FlickrParameterValues.MediumURL,
+//            Constants.FlickrParameterKeys.Format : Constants.FlickrParameterValues.ResponseFormat,
+//            Constants.FlickrParameterKeys.NoJSONCallback : Constants.FlickrParameterValues.DisableJSONCallback,
+//            Constants.FlickrParameterKeys.Lat : pinLocation!.latitude!,
+//            Constants.FlickrParameterKeys.Lon : pinLocation!.longitude!
+//        ]
+        //let outputURL : NSURL = createURLFromarameters()
+        print("Attempting to download")
+        //Alamofire.request("http://api.brewerydb.com/v2/categories/?key=\(Constants.BreweryParameterValues.APIKey)")
+        Alamofire.request("http://api.brewerydb.com/v2/beers?key=\(Constants.BreweryParameterValues.APIKey)&format=json&isOrganic=Y&styleId=1&withBreweries=Y")
+            .responseJSON { response  in
+                guard response.result.isSuccess else {
+                    print("failed")
+                    return
+                }
+                guard let responseJSON = response.result.value as? [String:AnyObject] else {
+                    print("Invalid tag informatiion")
+                    return
+                }
+                print(response)
+        return
+        }
+        
+//        parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! NSDictionary
+    }
     
+    func parseData(){
+    }
     
 }
 
 extension BreweryDBClient {
-    enum Constants {
+    
+    private func createURLFromParameters(paramenters: [String:AnyObject]) -> NSURL {
+        let components = NSURLComponents()
+        components.scheme = Constants.Brewery.APIScheme
+        components.host = Constants.Brewery.APIHost
+        components.path = Constants.Brewery.APIPath
+        components.queryItems = [NSURLQueryItem]() as [URLQueryItem]?
         
+        for (key, value) in paramenters {
+            let queryItem = NSURLQueryItem(name: key, value: "\(value)")
+            components.queryItems?.append(queryItem as URLQueryItem)
+        }
+        return components.url! as NSURL
+    }
+    
+    struct Constants {
+        struct Brewery {
+            static let APIScheme = "http"
+            static let APIHost = "api.brewerydb.com"
+            static let APIPath = "/services/rest/v2"
+        }
+        
+        struct BreweryParameterKeys {
+            static let Key = "key"
+            static let Format = "format"
+        }
+        
+        struct BreweryParameterValues {
+            static let APIKey = "8e63b90f589c3b3f2001c5e396f5d300"
+            static let FormatValue = "json"
+        }
     }
 }
--- BreweryTour/CatergoryViewController.swift
+++ BreweryTour/CatergoryViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let test = BreweryDBClient()
+        test.downloadBeerTypes()
     }
 
     override func didReceiveMemoryWarning() {
