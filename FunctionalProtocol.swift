9e0ea97c1094fe1656f4221571b89aeb8926af11
jamesjbot <jongs.j@gmail.com>
2016-10-09 05:37:16 -0400

    Testing viewcontrollers

---
+++ FunctionalProtocol.swift
@@ -0,0 +1,14 @@
+
+//
+//  FunctionalProtocol.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 10/9/16.
+//  Copyright © 2016 James Jongs. All rights reserved.
+//
+
+import Foundation
+
+protocol FunctionalProtocol {
+    func sayHi();
+}
