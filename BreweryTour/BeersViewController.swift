360adc72ed5ae184399d1681452aa91473a9341e
jamesjbot <jongs.j@gmail.com>
2016-10-12 17:12:24 -0400

    Add stubs for the datasource and delegate methods to BeersViewController

---
+++ BreweryTour/BeersViewController.swift
@@ -0,0 +1,51 @@
+//
+//  BeersViewController.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 10/12/16.
+//  Copyright © 2016 James Jongs. All rights reserved.
+//
+
+import UIKit
+
+class BeersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension BeersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+extension BeersViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
