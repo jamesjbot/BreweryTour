8343589febfebda18cd3d05668787a8f362ac78e
jamesjbot <jongs.j@gmail.com>
2016-10-15 18:40:40 -0400

    Properly display favorites and save favorites to the database

--- BreweryTour/BeerDetailViewController.swift
+++ BreweryTour/BeerDetailViewController.swift
@@ -41,6 +41,8 @@ class BeerDetailViewController: UIViewController {
         sender.setImage(image, for: .normal)
     }
     
+    // MARK: Variables
+    
     // TODO Must set favorite on initialization
     private var isBeerFavorited : Bool!
     
@@ -61,12 +63,17 @@ class BeerDetailViewController: UIViewController {
             availableText.text = "Availability: \(availText)"
         }
         breweryName.text = beer.brewer?.name
-        
-        // TODO Change this to beer's favorite status
-        isBeerFavorited = false
-        let image = UIImage(named: "heart_icon_black_white_line_art.png")
+        let image : UIImage?
+        // Change this to beer's favorite status
+        if beer.favorite {
+            isBeerFavorited = true
+            image = UIImage(named: "heart_icon.png")
+        } else {
+            isBeerFavorited = false
+            image = UIImage(named: "heart_icon_black_white_line_art.png")
+        }
         favoriteButton.setImage(image, for: .normal)
-        
+        deleteFromCoreData()
     }
 
     
@@ -83,27 +90,51 @@ class BeerDetailViewController: UIViewController {
     
     
     private func saveToCoreData() {
-        // TODO check to make sure it isn't already in the database
-        let beerNotInDB : Bool = false
-        if beerNotInDB {
-            Beer(id: beer.id!, name: beer.beerName!, beerDescription: beer.beerDescription!, availability: beer.availability!, context: (coreDataStack?.persistingContext)!)
-            do {
+        
+        // Check to make sure the Beer isn't already in the database
+        let request : NSFetchRequest<Beer> = NSFetchRequest(entityName: "Beer")
+        request.sortDescriptors = []
+        request.predicate = NSPredicate(format: "id = %@", argumentArray: [beer.id!])
+        do {
+            let results = try coreDataStack?.persistingContext.fetch(request)
+            if (results?.count)! == 0 {
+                let b = Beer(id: beer.id!, name: beer.beerName!, beerDescription: beer.beerDescription!, availability: beer.availability!, context: (coreDataStack?.persistingContext)!)
+                b.favorite = true
                 try coreDataStack?.persistingContext.save()
-            } catch {
-                fatalError("Error saving favorite")
             }
+        } catch {
+            fatalError("Error adding a beer")
         }
         
     }
     
     
     private func deleteFromCoreData() {
-        let beerNotInDB : Bool = false
-        if beerNotInDB {
-            return
-        } else {
-            // Get beer id delete in persistent
-            // Save persistent
+        print("attempting to delete")
+        // Check to make sure the Beer isn't already deleted in the database
+        let request : NSFetchRequest<Beer> = NSFetchRequest(entityName: "Beer")
+        request.sortDescriptors = []
+        print("beerid:\(beer.id)")
+        request.predicate = NSPredicate(format: "id == %@", argumentArray: [beer.id!])
+        do {
+            let results = try coreDataStack?.persistingContext.fetch(request)
+            //print("results:\(results)")
+            if (results?.count)! > 0 {
+                coreDataStack?.persistingContext.delete((results?[0])!)
+                try coreDataStack?.persistingContext.save()
+                print("Deleted")
+            }
+        } catch {
+            fatalError("Error deleting a beer")
+        }
+        do {
+            request.predicate = nil
+            let results = try coreDataStack?.persistingContext.fetch(request)
+            for i in results! as [Beer]{
+                print(i.id)
+            }
+        } catch {
+            
         }
     }
 
