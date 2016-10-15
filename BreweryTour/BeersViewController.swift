363d7de8d4da1d7c3039a80ec972537d2186ec86
jamesjbot <jongs.j@gmail.com>
2016-10-15 18:41:14 -0400

    Clean up comments and white space

--- BreweryTour/BeersViewController.swift
+++ BreweryTour/BeersViewController.swift
@@ -21,6 +21,7 @@ class BeersViewController: UIViewController {
     
     // MARK: IBOutlets
     
+
     @IBOutlet weak var tableView: UITableView!
     
     
@@ -61,20 +62,6 @@ class BeersViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
