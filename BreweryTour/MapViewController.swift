3b94f8a740b3d25f7e346a7a9a15ba16f5146cf4
jamesjbot <jongs.j@gmail.com>
2016-10-17 19:07:49 -0400

    Added code to favorite breweries on the map. Also enable a refresh of the map correctly when favorite breweries are deleted.

--- BreweryTour/MapViewController.swift
+++ BreweryTour/MapViewController.swift
@@ -22,11 +22,12 @@ class MapViewController : UIViewController, NSFetchedResultsControllerDelegate {
     private var incomingLocations = [Brewery]()
     private var frc : NSFetchedResultsController<Brewery> = NSFetchedResultsController()
     private let locationManager = CLLocationManager()
-    private let coreDataStack = (UIApplication.shared.delegate as! AppDelegate).coreDataStack
+    
+    fileprivate let coreDataStack = (UIApplication.shared.delegate as! AppDelegate).coreDataStack
     
     
     // MARK: Functions
-    
+    // Get the Brewery entries from the database
     private func initializeFetchAndFetchBreweries(){
         let request : NSFetchRequest<Brewery> = NSFetchRequest(entityName: "Brewery")
         request.sortDescriptors = []
@@ -40,9 +41,8 @@ class MapViewController : UIViewController, NSFetchedResultsControllerDelegate {
     
     override func viewDidLoad(){
         super.viewDidLoad()
-        
-        initializeFetchAndFetchBreweries()
-        populateMap()
+        print("View did load called")
+
         
         // CoreLocation initialization for user location
         locationManager.delegate = self
@@ -51,11 +51,23 @@ class MapViewController : UIViewController, NSFetchedResultsControllerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
             locationManager.requestLocation()
         }
-        
     }
-        
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear called")
+        mapView.removeAnnotations(mapView.annotations)
+        print("Mapview now has \(mapView.annotations.count) annotations")
+        initializeFetchAndFetchBreweries()
+        populateMap()
+    }
+    
+    
+    // Puts all the Brewery entries on to the map
     private func populateMap(){
+        print("populateMapCalled")
+        // Remove all the annotation and repopulate
+        mapView.removeAnnotations(mapView.annotations)
         var annotations = [MKAnnotation]()
         for i in incomingLocations {
             guard i.latitude != nil && i.longitude != nil else {
@@ -65,7 +77,7 @@ class MapViewController : UIViewController, NSFetchedResultsControllerDelegate {
             // TODO Check if the point is a favorite is so populate accordingly
             
             let aPin = MKPointAnnotation()
-            print("Last Brewery: \(i)")
+            //print("Last Brewery: \(i)")
             aPin.coordinate = CLLocationCoordinate2D(latitude: Double(i.latitude!)!, longitude: Double(i.longitude!)!)
             aPin.title = i.name
             aPin.subtitle = i.url
@@ -74,13 +86,51 @@ class MapViewController : UIViewController, NSFetchedResultsControllerDelegate {
         mapView.addAnnotations(annotations)
         _ = mapView.userLocation
     }
+
+    fileprivate func findBreweryInCoreData(by : MKAnnotation) -> Brewery? {
+        let lat = by.coordinate.latitude
+        let lon = by.coordinate.longitude
+        // Iterate across Brewery object on the map
+        for i in incomingLocations {
+            if i.latitude == (lat.description) && i.longitude == (lon.description) {
+                print("found brewery in coredata")
+                return i
+            }
+        }
+        return nil
+    }
+
+
+    fileprivate func findBreweryinFavorites(by: MKAnnotation) -> Brewery? {
+        let lat = by.coordinate.latitude
+        let lon = by.coordinate.longitude
+        let request : NSFetchRequest<Brewery> = NSFetchRequest(entityName: "Brewery")
+        request.sortDescriptors = []
+        let frc = NSFetchedResultsController(fetchRequest: request,
+                                                      managedObjectContext: (coreDataStack?.favoritesContext)!,
+                                                      sectionNameKeyPath: nil, cacheName: nil)
+        do {
+            try frc.performFetch()
+        } catch {
+            fatalError()
+        }
+        for i in frc.fetchedObjects! as [Brewery] {
+            if i.latitude == (lat.description) && i.longitude == (lon.description) {
+                print("Found brewery in favorites")
+                return i
+            }
+        }
+        return nil
+    }
+    
 }
 
 
 extension MapViewController : MKMapViewDelegate {
 
-    // This format pins (annotations on the map)
+    // This formats the pins and calloutAccessory views on the map
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        print("Formatting of pins and callouts")
         let reuseId = "pin"
         var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
         if pinView == nil {
@@ -92,13 +142,31 @@ extension MapViewController : MKMapViewDelegate {
             } else {
                 pinView!.pinTintColor = UIColor.red
             }
-            // Here is how i put in the callout
-            pinView?.tintColor = UIColor.gray
-            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-            pinView!.leftCalloutAccessoryView = UIButton(type: .contactAdd)
-            pinView!.detailCalloutAccessoryView?.backgroundColor = UIColor.red
+            
+            // Format annotation callouts here
+            pinView?.tintColor = UIColor.red
+            //pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            //pinView!.leftCalloutAccessoryView = UIButton(type: .contactAdd)
+            pinView?.canShowCallout = true
+            let foundBrewery = findBreweryinFavorites(by: annotation)
+            if foundBrewery != nil {
+                print("Formating pin, \(annotation.title) This brewery has been favorited")
+                let temp = UIImage(named: "small_heart_icon.png")
+                let localButton = UIButton(type: .contactAdd)
+                localButton.setImage(temp, for: .normal)
+                pinView?.leftCalloutAccessoryView = localButton
+                //(pinView?.leftCalloutAccessoryView as! UIButton).isSelected = true
+            } else {
+                print("Formatting pin, \(annotation.title) This brewery is unseleted")
+                let favoriteBreweryButton = UIButton(type: .contactAdd)
+                //favoriteBreweryButton.imageView?.image =  UIImage(named: "small_heart_icon_black_white_line_art.png")
+                //let temp = UIImage(named: "small_heart_icon_black_white_line_art.png")
+                //favoriteBreweryButton.setImage(temp, for: .normal)
+                pinView!.leftCalloutAccessoryView = favoriteBreweryButton
+            }
+            //pinView!.detailCalloutAccessoryView?.backgroundColor = UIColor.red
             //pinView?.detailCalloutAccessoryView? = (UIButton(type: .roundedRect))
-        } else {
+        } else { // TODO why would I not reformat the pinView no matter what
             pinView!.annotation = annotation
         }
         return pinView
@@ -143,7 +211,7 @@ extension MapViewController : MKMapViewDelegate {
     // Display the route on map
     func displayRouteOnMap(route: MKRoute){
         mapView.add(route.polyline)
-        mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsetsMake(10.0,10.0,10.0,10.0), animated: true)
+        mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsetsMake(100.0,100.0,100.0,100.0), animated: true)
     }
     
     
@@ -159,6 +227,30 @@ extension MapViewController : MKMapViewDelegate {
         if control == view.leftCalloutAccessoryView {
             print("Left accessory view called")
             // TODO Add to favorites Brewery
+            // If this is already a favorite do nothing for now
+            let favBrewery = findBreweryinFavorites(by: view.annotation!)
+            guard favBrewery == nil else { // A favorite encountered no need to add it again.
+                print("This has already been favorited")
+                return
+            }
+            // TODO Prompt the user that they added this brewery to favorites
+            // Find the brewery object that belongs to this location
+            let targetBrewery : Brewery = findBreweryInCoreData(by: view.annotation!)!
+            // Create a brewery object in the favorites
+            Brewery(inBrewery: targetBrewery, context: (coreDataStack?.favoritesContext)!)
+            // TODO See if we can implement this in coredata
+            do {
+                
+                try coreDataStack?.favoritesContext.save()
+                
+                DispatchQueue.main.async {
+                    (view.leftCalloutAccessoryView as! UIButton).setImage(UIImage(named: "heart_icon.png"), for: .normal)
+                    view.setNeedsDisplay()
+                    print("Favorite Created")
+                }
+            } catch {
+                
+            }
         } else if control == view.rightCalloutAccessoryView {
             print("Right accesory view called")
             // Goto Webpage Information
@@ -169,6 +261,11 @@ extension MapViewController : MKMapViewDelegate {
                 }
             }
         }
+    }
+    
+    // Function to performUIUpdates on main queue
+    private func performUpdatesOnMain(updates: () -> Void) {
+
     }
     
     
