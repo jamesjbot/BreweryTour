c5657a70e78b6acee874877982a79b26106aaffb
jamesjbot <jongs.j@gmail.com>
2016-10-17 19:05:28 -0400

    Added favorite breweries view controller class

---
+++ BreweryTour/FavoriteBreweriesViewController.swift
@@ -0,0 +1,154 @@
+//
+//  FavoriteBreweriesViewController.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 10/17/16.
+//  Copyright © 2016 James Jongs. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FavoriteBreweriesViewController: UIViewController {
+    
+    
+    // MARK: Constants
+    
+    fileprivate let coreDataStack = (UIApplication.shared.delegate as! AppDelegate).coreDataStack
+    
+    // MARK: Variables
+    
+    fileprivate var fetchedResultsController : NSFetchedResultsController<Brewery>
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("favorites tab enetered")
+        // Do any additional setup after loading the view.
+        perfromFetchOnResultsController()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        let request : NSFetchRequest<Brewery> = NSFetchRequest(entityName: "Brewery")
+        request.sortDescriptors = []
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
+                                                              managedObjectContext: (coreDataStack?.favoritesContext)!,
+                                                              sectionNameKeyPath: nil,
+                                                              cacheName: nil)
+        super.init(coder: aDecoder)
+    }
+    
+    
+    private func perfromFetchOnResultsController(){
+        fetchedResultsController.delegate = self
+        // Create a request for Brewery objects and fetch the request from Coredata
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("There was a problem fetching from coredata")
+        }
+    }
+    
+    
+    func configureCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+        guard let selectedObject = fetchedResultsController.object(at: indexPath as IndexPath) as Brewery? else { fatalError("Unexpected Object in FetchedResultsController") }
+        // Populate cell from the NSManagedObject instance
+        cell.textLabel?.text = selectedObject.name
+        //cell.detailTextLabel?.text = selectedObject.brewer?.name
+//        if let data : NSData = (selectedObject.image) {
+//            let im = UIImage(data: data as Data)
+//            cell.imageView?.image = im
+//        }
+    }
+}
+
+extension FavoriteBreweriesViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        print("will change called")
+        tableView.beginUpdates()
+    }
+    
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        print("didChange called")
+        switch (type){
+        case .insert:
+            tableView.insertRows(at: [newIndexPath! as IndexPath], with: UITableViewRowAnimation.fade)
+            break
+        case .delete:
+            tableView.deleteRows(at: [indexPath! as IndexPath], with: UITableViewRowAnimation.fade)
+            break
+        case .move:
+            break
+        case .update:
+            configureCell(cell: tableView.cellForRow(at: indexPath!)!, indexPath: indexPath! as NSIndexPath)
+            break
+        }
+    }
+    
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+        tableView.reloadData()
+        print("finished")
+    }
+}
+
+
+extension FavoriteBreweriesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("called favoritesviewcontroller \(fetchedResultsController.fetchedObjects?.count)")
+        return (fetchedResultsController.fetchedObjects?.count)!
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell from the tableview and populate with name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
+    }
+    
+}
+
+extension FavoriteBreweriesViewController : UITableViewDelegate {
+    
+    @objc(tableView:commitEditingStyle:forRowAtIndexPath:) func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let object = (fetchedResultsController.fetchedObjects! as [Brewery])[indexPath.row]
+            coreDataStack?.favoritesContext.delete(object)
+            do {
+                try coreDataStack?.favoritesContext.save()
+            } catch {
+                fatalError("Error deleting row")
+            }
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Create target viewcontroller
+        // Selecting a brewery should go to the map
+//        let destinationViewcontroller = storyboard?.instantiateViewController(withIdentifier: "BeerDetailViewController") as! BeerDetailViewController
+//        
+//        // Push beer information to Detail View Controller
+//        destinationViewcontroller.beer = fetchedResultsController.fetchedObjects?[indexPath.row]
+//        
+//        // Segue to view controller
+//        navigationController?.pushViewController(destinationViewcontroller, animated: true)
+    }
+}
+
+
