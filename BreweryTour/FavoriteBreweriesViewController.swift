3c5d8905f6cf304102a46856c96b353a416f6f19
jamesjbot <jongs.j@gmail.com>
2016-11-15 18:08:31 -0500

    Added comments and accommodate completion handler on mediator call.

--- BreweryTour/FavoriteBreweriesViewController.swift
+++ BreweryTour/FavoriteBreweriesViewController.swift
@@ -164,10 +164,13 @@ extension FavoriteBreweriesViewController : UITableViewDelegate {
     }
     
     
+    // When we select a favorite brewery lets zoom to that brewery on the map
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // Send the brewery to the mediator, then switch to the map tab
          // TODO I temporarily inserted a false this is wrong.
-        Mediator.sharedInstance().selected(thisItem: frc.object(at: indexPath))
+        Mediator.sharedInstance().selected(thisItem: frc.object(at: indexPath)) {
+            (success, msg) -> Void in
+        }
         self.tabBarController?.selectedIndex = 0
     }
 }
