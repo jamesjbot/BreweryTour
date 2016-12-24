c4e31994594d93a15261c69a645389b89719f38f
jamesjbot <jongs.j@gmail.com>
2016-12-23 23:31:34 -0500

    Added statistics to deletion

--- BreweryTour/Settings.swift
+++ BreweryTour/Settings.swift
@@ -15,6 +15,10 @@ class Settings: UIViewController {
     // MARK: Constants
     let coreDataStack = (UIApplication.shared.delegate as! AppDelegate).coreDataStack
     let container = (UIApplication.shared.delegate as! AppDelegate).coreDataStack?.container
+    let beerFetch: NSFetchRequest<Beer> = Beer.fetchRequest()
+    let breweryFetch: NSFetchRequest<Brewery> = Brewery.fetchRequest()
+    let styleFetch: NSFetchRequest<Style> = Style.fetchRequest()
+
     // MARK: IBOutlet
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var activityIndic: UIActivityIndicatorView!
@@ -27,24 +31,21 @@ class Settings: UIViewController {
             self.startIndicator()
             container?.performBackgroundTask({ (context) in
                 var success: Bool = true
-                let beerFetch: NSFetchRequest<Beer> = Beer.fetchRequest()
-                var request = NSBatchDeleteRequest(fetchRequest: beerFetch as! NSFetchRequest<NSFetchRequestResult>)
+                var request = NSBatchDeleteRequest(fetchRequest: self.beerFetch as! NSFetchRequest<NSFetchRequestResult>)
                 do {
                     try context.execute(request)
                 } catch let error {
                     self.displayAlertWindow(title: "Error", msg: "Error deleting Beer data \(error)")
                     success = false
                 }
-                let styleFetch: NSFetchRequest<Style> = Style.fetchRequest()
-                request = NSBatchDeleteRequest(fetchRequest: styleFetch as! NSFetchRequest<NSFetchRequestResult>)
+                request = NSBatchDeleteRequest(fetchRequest: self.styleFetch as! NSFetchRequest<NSFetchRequestResult>)
                 do {
                     try context.execute(request)
                 } catch let error {
                     self.displayAlertWindow(title: "Error", msg: "Error deleting Style data \(error)")
                     success = false
                 }
-                let breweryFetch: NSFetchRequest<Brewery> = Brewery.fetchRequest()
-                request = NSBatchDeleteRequest(fetchRequest: breweryFetch as! NSFetchRequest<NSFetchRequestResult>)
+                request = NSBatchDeleteRequest(fetchRequest: self.breweryFetch as! NSFetchRequest<NSFetchRequestResult>)
                 do {
                     try context.execute(request)
                 } catch let error {
@@ -55,33 +56,38 @@ class Settings: UIViewController {
                 do {
                     try context.save()
                 } catch let error {
+                    success = false
                     self.displayAlertWindow(title: "Error", msg: "Successfully deleted but unable to save?")
                 }
-                do {
-                    let beerCount =  try context.fetch(beerFetch).count
-                    let styleCount = try context.fetch(styleFetch).count
-                    let breweryCount = try context.fetch(breweryFetch).count
-                    if success == true {
-                        self.displayAlertWindow(title: "Delete Data", msg: "Successful\nStyles:\(styleCount)\nBeers\(beerCount)\nBreweries:\(breweryCount)")
-                    }
-                } catch let error {
-                    self.displayAlertWindow(title: "Error", msg: "Please try again.")
+                if success == true {
+                    self.displayAlertWindow(title: "Delete Data", msg: "Successful"+self.statistics())
                 }
-                
-
             })
         }
         let action = UIAlertAction(title: "Delete",
                                    style: .default,
                                    handler: deleteAll)
         displayAlertWindow(title: "Delete All Data",
-                           msg: "Are you sure you want to delete all data, this includes tasting notes and favorites?",
+                           msg: "Are you sure you want to delete all data, this includes\ntasting notes and favorites?"+statistics(),
                            actions: [action])
         //activityIndic.isHidden = false
     }
     
     
     // MARK: - Functions
+    
+    func statistics() -> String {
+        do {
+            let beerCount =  try container?.viewContext.fetch(beerFetch).count
+            let styleCount = try container?.viewContext.fetch(styleFetch).count
+            let breweryCount = try container?.viewContext.fetch(breweryFetch).count
+            return "\nStyles:\(styleCount!)\nBeers\(beerCount!)\nBreweries:\(breweryCount!)"
+        } catch let error {
+            self.displayAlertWindow(title: "Error", msg: "Please try again.")
+            return ""
+        }
+    }
+    
     
     func startIndicator() {
         DispatchQueue.main.async {
