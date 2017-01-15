d5119e009c1b8f5baf5146917af1292376034e4d
jamesjbot <jongs.j@gmail.com>
2017-01-15 06:27:05 -0500

    Added message constants.

--- BreweryTour/Observer.swift
+++ BreweryTour/Observer.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2016 James Jongs. All rights reserved.
 //
 
+struct Message {
+    static let Reload = "reload data"
+    static let FetchError = "Error fetching data"
+    static let Retry = "Failed to download initial styles\ncheck network connection and try again."
+}
+
 protocol Observer {
+
+    // MARK: Constant
+
+
     func sendNotify(from: AnyObject, withMsg msg: String )
 }
