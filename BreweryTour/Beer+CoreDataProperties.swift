eef05772a1fab5d453a5caf7fba372794f3ef7fa
jamesjbot <jongs.j@gmail.com>
2017-01-16 05:05:15 -0500

    Commit new version

--- BreweryTour/Beer+CoreDataProperties.swift
+++ BreweryTour/Beer+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Beer+CoreDataProperties.swift
 //  
 //
-//  Created by James Jongsurasithiwat on 10/12/16.
+//  Created by James Jongsurasithiwat on 1/16/17.
 //
 //
 
@@ -16,10 +16,20 @@ extension Beer {
         return NSFetchRequest<Beer>(entityName: "Beer");
     }
 
+    @NSManaged public var abv: String?
     @NSManaged public var availability: String?
     @NSManaged public var beerDescription: String?
-    @NSManaged public var name: String?
-    @NSManaged public var style: String?
+    @NSManaged public var beerName: String?
+    @NSManaged public var breweryID: String?
+    @NSManaged public var favorite: Bool
+    @NSManaged public var ibu: String?
+    @NSManaged public var id: String?
+    @NSManaged public var image: NSData?
+    @NSManaged public var imageUrl: String?
+    @NSManaged public var isOrganic: Bool
+    @NSManaged public var styleID: String?
     @NSManaged public var tastingNotes: String?
+    @NSManaged public var brewer: Brewery?
+    @NSManaged public var stylefamily: Style?
 
 }
