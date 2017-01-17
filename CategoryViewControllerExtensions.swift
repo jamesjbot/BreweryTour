dfa8e922d2ef66989deef0c6c0d609b16af18daa
jamesjbot <jongs.j@gmail.com>
2017-01-16 23:01:01 -0500

    Added higher resolution beer icons.

---
+++ CategoryViewControllerExtensions.swift
@@ -0,0 +1,10 @@
+//
+//  CategoryViewControllerExtensions.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 1/16/17.
+//  Copyright © 2017 James Jongs. All rights reserved.
+//
+
+import Foundation
+
