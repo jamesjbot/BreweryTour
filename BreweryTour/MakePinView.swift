87bb6b4d26e6fdf45da0742341a2ef7ed33d2b9f
jamesjbot <jongs.j@gmail.com>
2017-06-03 06:26:20 -0400

    fix: Update comments in MakePinView and simplify code for readability.
    Improves readability

--- BreweryTour/MakePinView.swift
+++ BreweryTour/MakePinView.swift
@@ -24,81 +24,95 @@ class MakePinView {
         return Singleton.sharedInstance
     }
 
-    
-    internal func makePinOrBeerMugViewOnMap(fromAnnotationView oldPinView: MKAnnotationView?,
-                              fromAnnotation annotation: MKAnnotation,
+    // Return the MKAnnotationView for the map based on what is needed.
+    internal func makePinOrBeerMugViewOnMap(
+                              fromAnnotationView oldPinView: MKAnnotationView?,
+                              fromAnnotation inputAnnotation: MKAnnotation,
                               withUserLocation: MKUserLocation,
                               floatingAnnotation: MKAnnotation?,
                               reuseID: String,
                               brewery: Brewery?) -> MKAnnotationView {
-        // The states are 
-        // What kind of MKAnnotationViews can we have
+
+        // Return types: What kind of oldPinViews (dequeued MKAnnotationViews) could we have
         // 1) an MKPinAnnotationView
         // 2) an MKAnnotationView
-        // 3) Neither of the View (nil need to create it based on annotation)
+        // 3) Neither of these Views (nil need to create it based on input annotation type)
 
-        // What kind of MKAnnotation could I have
-        // Here 1 and 2 use the same kind of pin annotation
+        // Input annotation types: What kind of MKAnnotations do we have to map
+        // Note: Here cases 1 and 2 use the same kind of pin annotation
         // 1) MKUserLocation
         // 2) FloatingLocation
         // 3) Regular beer location
 
-        // Process 1 & 2 MkAnnotations (MKUserLocation && FloatingLocation)
-        if annotation is MKUserLocation || annotation === floatingAnnotation {
-            // Just user and floating pins pins
-            // FIXME: remove this dead code.
-            // What is this actually doing now
+        // Switch on type of Annotation we are trying to map
+        // Process Case 1 & Case 2 MkAnnotations (MKUserLocation && FloatingLocation)
+        if inputAnnotation is MKUserLocation || inputAnnotation === floatingAnnotation {
+            // Just user and floating pins.
+
+            // We must have an MKAnnotationView and be an MKPinAnnotationView
             guard let localOldPinView = oldPinView,
-                // We have a an MKAnnotationView
-                    oldPinView is MKPinAnnotationView else {
-                        // We do not have an old pin
-                        // Why are we trying to make an oldPinView again
-                        //return localOldPinView
-                        //fatalError()
-                        //return
-                        // Create a new BeerMug Annotation
-                        //If the annotation is a user it should never be a beer
-                return createUserPinAnnotationView(annotation: annotation,
-                                               reuseID: reuseID,
-                                               isfloatingAnnotation: annotation === floatingAnnotation)
+                oldPinView is MKPinAnnotationView else {
+                    // Else we do not have a reusable MKAnnotationView
+                    // Create a new User Pin Annotation or floating annotation view
+                    return createUserPinAnnotationView(annotation: inputAnnotation,
+                                                       reuseID: reuseID,
+                                                       isfloatingAnnotation: inputAnnotation === floatingAnnotation)
             }
             
             // Modify the dequeued MKAnnotationView (oldPinView)
-            if annotation === floatingAnnotation {
-                (oldPinView as! MKPinAnnotationView).pinTintColor = UIColor.magenta
-            } else {
-                (oldPinView as! MKPinAnnotationView).pinTintColor = UIColor.black
-            }
-            return oldPinView!
+            return modifyColorOf(MKAnnotationPinView: localOldPinView,
+                                 basedOn: inputAnnotation,
+                                 withSelectedAnnotation: floatingAnnotation)
 
-        } else { // Regular beer location, process 3 MKAnnotations
+        } else { // Process Case 3 MKAnnotations (Regular brewery location)
 
             guard let oldPinView = oldPinView else {
-                return createBeermugAnnnotationView(annotation: annotation, reuseID: reuseID, brewery: brewery)
+                return createBeermugAnnnotationView(withAnnotation: inputAnnotation,
+                                                    reuseID: reuseID,
+                                                    brewery: brewery)
             }
-            return oldPinView // no need to change the image it's already a beer
+            return oldPinView // no need to change the image it's already a beer mug
         }
     }
 
+    // Modifies the color of a Dequeued MKAnnotationPinView that represent the center of 
+    // where the user wants beer locations clustered around.
+    private func modifyColorOf(MKAnnotationPinView oldPinView: MKAnnotationView,
+                               basedOn annotation: MKAnnotation,
+                               withSelectedAnnotation floatingAnnotation: MKAnnotation?) -> MKPinAnnotationView {
+        assert(oldPinView is MKPinAnnotationView)
+        let pinView = oldPinView as? MKPinAnnotationView
+        if annotation === floatingAnnotation {
+            pinView?.pinTintColor = UIColor.magenta
+        } else {
+            pinView?.pinTintColor = UIColor.black
+        }
+        return pinView!
+    }
+
+
     // Creates AnnotationView with pins
-    private func createUserPinAnnotationView(annotation: MKAnnotation, reuseID: String, isfloatingAnnotation: Bool) -> MKPinAnnotationView {
+    private func createUserPinAnnotationView(annotation: MKAnnotation,
+                                             reuseID: String,
+                                             isfloatingAnnotation: Bool) -> MKPinAnnotationView {
         let pinView = JustUserAndFloatingPinAnnotationView(annot: annotation, reuse: reuseID)
         if isfloatingAnnotation {
             pinView.pinTintColor = UIColor.magenta
-            return pinView
         } else { // This is userlocation
             pinView.pinTintColor = UIColor.black
-            return pinView
         }
+        return pinView
+
     }
 
     // Creates AnnotationView with little beer mugs
-    private func createBeermugAnnnotationView(annotation: MKAnnotation, reuseID: String, brewery: Brewery?) -> MKAnnotationView {
-        let mkView = BeermugAnnotationView(annot: annotation,
-                                      reuse: reuseID,
-                                      brewery: brewery,
-                                      favorite: (brewery?.favorite)!)
-        return mkView
+    private func createBeermugAnnnotationView(withAnnotation: MKAnnotation,
+                                              reuseID: String,
+                                              brewery: Brewery?) -> MKAnnotationView {
+        return BeermugAnnotationView(annot: withAnnotation,
+                                           reuse: reuseID,
+                                           brewery: brewery,
+                                           favorite: (brewery?.favorite)!)
     }
 
 }
