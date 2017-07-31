2815495295f91f8cdb1985bece0dc5d4020a2fe6
jamesjbot <jongs.j@gmail.com>
2017-07-31 09:55:32 -0400

    docs: Improve readability.

--- BreweryTour/XcodegeneratedFiles/Beer+CoreDataClass.swift
+++ BreweryTour/XcodegeneratedFiles/Beer+CoreDataClass.swift
@@ -12,23 +12,31 @@ import SwiftyBeaver
 
 
 public class Beer: NSManagedObject {
+
     convenience init(id: String,
                      name: String,
                      beerDescription: String,
                      availability: String,
                      context : NSManagedObjectContext){
+
         let entityDescription = NSEntityDescription.entity(forEntityName: "Beer", in: context)
+
         self.init(entity: entityDescription!, insertInto: context)
+
         self.beerName = name
         self.beerDescription = beerDescription
         self.availability = availability
         self.id = id
     }
 
-    convenience init(data: BeerData, context: NSManagedObjectContext) throws {
+    convenience init(data: BeerData,
+                     context: NSManagedObjectContext) throws {
+
         if let entityDescription = NSEntityDescription.entity(forEntityName: "Beer", in: context) {
+
             self.init(entity: entityDescription,
                       insertInto: context)
+
             availability = data.availability
             beerDescription = data.beerDescription
             beerName = data.beerName
@@ -57,7 +65,9 @@ public class Beer: NSManagedObject {
                      style : String?,
                      context: NSManagedObjectContext){
         let entityDescription = NSEntityDescription.entity(forEntityName: "Beer", in: context)
+
         self.init(entity: entityDescription!, insertInto: context)
+
         self.beerName = name
         self.beerDescription = description
         self.availability = availability
