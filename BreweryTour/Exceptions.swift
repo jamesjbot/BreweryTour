87f6a8974c9ed7ca85c6d406f3fcb1f300ecdc13
jamesjbot <jongs.j@gmail.com>
2017-09-05 08:13:53 -0400

    feature: Start and Error/Exceptions file

---
+++ BreweryTour/Exceptions.swift
@@ -0,0 +1,13 @@
+//
+//  Exceptions.swift
+//  BreweryTour
+//
+//  Created by James Jongsurasithiwat on 7/25/17.
+//  Copyright © 2017 James Jongs. All rights reserved.
+//
+
+import Foundation
+
+enum BreweryTourErrors: Error {
+    case UnableToCreateBeerException
+}
