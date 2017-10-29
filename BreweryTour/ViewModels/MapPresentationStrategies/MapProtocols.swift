f55728701443b84ca9927cf6d93eb97fefbfc9e0
jamesjbot <jongs.j@gmail.com>
2017-10-29 14:58:25 -0400

    style: Clean up whitespace, add comments, organize code.

--- BreweryTour/ViewModels/MapPresentationStrategies/MapProtocols.swift
+++ BreweryTour/ViewModels/MapPresentationStrategies/MapProtocols.swift
@@ -5,6 +5,7 @@
 //  Created by James Jongsurasithiwat on 1/14/17.
 //  Copyright © 2017 James Jongs. All rights reserved.
 //
+
 /*
     The base class for map strategies, provides basic functions for subclasses.
  */
@@ -13,45 +14,52 @@ import Foundation
 import MapKit
 import SwiftyBeaver
 
+
 protocol MapAnnotationProvider {
-    // converts breweries to annotations
-    func convertLocationToAnnotation(breweries: [Brewery]) -> [MKAnnotation] // this is an internal helper function that is called
-    func endSearch() // Called from mapviewcontroller
+
+    /// Converts breweries to annotations
+    func convertBreweryToAnnotation(breweries: [Brewery]) -> [MKAnnotation]
+
+    /// Gets current breweries from the strategy
+    func getBreweries() -> [Brewery]
+
+    /// This function is called by the MapAnnotationReceiver to end the current search
+    func endSearch() -> (()->())?
+
+    /// Sends annotation to MapAnnotation
     func send(annotations: [MKAnnotation], to map: MapAnnotationReceiver)
 }
 
-// FIXME: This is my new protocl
-protocol MappableStrategy {
-    func endSearch()
-    func sortLocations() -> [Brewery]?
+
+protocol MappableStrategy: MapAnnotationProvider {
+
+    var parentMapViewController: MapAnnotationReceiver? { set get }
+    var targetLocation: CLLocation? { set get }
+
+    func sortLocations(_: [Brewery]) -> [Brewery]?
+
+    init()
+
+    init(view: MapAnnotationReceiver)
 }
+
+
+// MARK: -
 
 extension MappableStrategy {
-    internal func endSearch() {
-        fatalError("You must override endSearch()!!!")
-        // Dummy stub for BreweryMapStrategy
-        // StyleMapStrategy will override it's implementation
-    }
-}
 
-
-class MapStrategy: NSObject, MapAnnotationProvider, MappableStrategy {
-
-    // MARK: - Variables
-
-    var breweryLocations: [Brewery] = []
-    var parentMapViewController: MapAnnotationReceiver? = nil
-    var targetLocation: CLLocation? = nil
-
-
-    // MARK: - Functions
+    // MARK: Functions
 
     init(view: MapAnnotationReceiver){
+        self.init()
         parentMapViewController = view
     }
 
+
+    // MARK: Protocol Extension implementation of MapAnnotationProvider functions
+
     // Converts brewery locations to map annotations
-    func convertLocationToAnnotation(breweries breweryLocations: [Brewery]) -> [MKAnnotation] {
+    func convertBreweryToAnnotation(breweries breweryLocations: [Brewery]) -> [MKAnnotation] {
         var annotations = [MKAnnotation]()
         for breweryLocation in breweryLocations {
 
@@ -71,37 +79,31 @@ class MapStrategy: NSObject, MapAnnotationProvider, MappableStrategy {
     }
 
 
-    internal func endSearch() {
-        fatalError("You must override endSearch()!!!")
-        // Dummy stub for BreweryMapStrategy
-        // StyleMapStrategy will override it's implementation
-    }
-
-
+    /// Sends annotation to MapAnnotation
     func send(annotations: [MKAnnotation], to map: MapAnnotationReceiver) {
-        // Format the first maximumClosestBreweries for display.
+
         map.updateMap(withAnnotations: annotations)
-        //parentMapViewController?.updateMap(withAnnotations: convertLocationToAnnotation(breweries: <#T##[Brewery]#>))
     }
 
     
     // Sort the breweries by distance to targetLocation
-    func sortLocations() -> [Brewery]? {
+    func sortLocations(_ input: [Brewery]) -> [Brewery]? {
+
+        var breweryLocations = input
         // If there are less than 2 breweries no need to sort.
         guard breweryLocations.count > 1 else {
             return breweryLocations
         }
         guard isAllLocationDataNonNil(in: breweryLocations) else {
-            SwiftyBeaver.error("MapStrategy detected breweries without location data.")
+            //SwiftyBeaver.error("MapStrategy detected breweries without location data.")
             return nil
         }
-        SwiftyBeaver.info("MapStrategy.sortLocations() Sorting breweries by positons")
+        //SwiftyBeaver.info("MapStrategy.sortLocations() Sorting breweries by positons")
         breweryLocations = breweryLocations.sorted(by:
             { (brewery1, brewery2) -> Bool in
-                SwiftyBeaver.verbose("MapStrategy within the brewery sorting closures working on \(breweryLocations.count) breweries")
                 let location1: CLLocation = CLLocation(latitude: CLLocationDegrees(Double(brewery1.latitude!)!), longitude: CLLocationDegrees(Double(brewery1.longitude!)!))
                 let location2: CLLocation = CLLocation(latitude: CLLocationDegrees(Double(brewery2.latitude!)!), longitude: CLLocationDegrees(Double(brewery2.longitude!)!))
-                return ((targetLocation?.distance(from: location1))! as Double) < ((targetLocation?.distance(from: location2))! as Double)
+                return ((targetLocation!.distance(from: location1)) as Double) < ((targetLocation!.distance(from: location2)) as Double)
         })
 
         return breweryLocations
