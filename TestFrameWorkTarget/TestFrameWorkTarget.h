234feff30bee1ab238b11cfc9632a83610fb2502
jamesjbot <jongs.j@gmail.com>
2017-11-10 02:48:10 -0500

    chore: Delete test framework

---
+++ TestFrameWorkTarget/TestFrameWorkTarget.h
@@ -0,0 +1,19 @@
+//
+//  TestFrameWorkTarget.h
+//  TestFrameWorkTarget
+//
+//  Created by James Jongsurasithiwat on 11/10/17.
+//  Copyright © 2017 James Jongs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFrameWorkTarget.
+FOUNDATION_EXPORT double TestFrameWorkTargetVersionNumber;
+
+//! Project version string for TestFrameWorkTarget.
+FOUNDATION_EXPORT const unsigned char TestFrameWorkTargetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameWorkTarget/PublicHeader.h>
+
+
